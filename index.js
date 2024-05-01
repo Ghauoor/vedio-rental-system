a93788e1506621c3b879517a43f86a538accd2f0
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-05-01 19:40:25 +0500

    Add Logging System

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require("express-async-errors");
+require("winston-mongodb");
 const { configDotenv } = require("dotenv");
 configDotenv();
 const express = require("express");
@@ -8,9 +10,11 @@ const movies = require("./src/routes/movies");
 const rentals = require("./src/routes/rentals");
 const users = require("./src/routes/users");
 const auth = require("./src/routes/auth");
+const error = require("./src/middlewares/error");
+const winston = require("winston");
 
-const app = express();
 connectionDB();
+const app = express();
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
@@ -18,6 +22,28 @@ app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
+app.use(error);
+
+winston.configure({
+  transports: [
+    new winston.transports.File({ filename: "logfile.log" }),
+    new winston.transports.MongoDB({
+      db: process.env.MONGO_URL,
+      options: { useUnifiedTopology: true },
+      collection: "errorLog",
+    }),
+  ],
+});
+
+process.on("uncaughtException", (ex) => {
+  console.log("WE GOT UNCAUGHT EXCEPTION");
+  winston.error(ex.message, ex);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (ex) => {
+  throw ex;
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("App is Listening to the Port " + PORT));
