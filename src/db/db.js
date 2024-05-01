dd8b4d27edf324b840fc2c13c2b9dd91f6acf128
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-05-01 23:39:39 +0500

    Spell Correction

--- src/db/db.js
+++ src/db/db.js
@@ -1,8 +1,8 @@
-const mongooes = require("mongoose");
+const mongoose = require("mongoose");
 
 const connectionDB = async () => {
   try {
-    const conn = await mongooes.connect(process.env.MONGO_URL);
+    const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB connect: ${conn.connection.host}`);
   } catch (error) {
     console.log(error.message);
